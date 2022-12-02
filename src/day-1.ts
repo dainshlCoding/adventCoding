0e3b715c000eb921f02d755bd7322efc9b8814fd
DainshlBA <daniel.kreppein@gmx.net>
2022-12-02 08:23:12 +0100

    day 1

--- src/day-1.ts
+++ src/day-1.ts
@@ -1,1 +1,21 @@
-console.log('HI')
+import * as fs from 'fs';
+
+const input = fs.readFileSync('src/input-1.txt', 'utf8').toString();
+const data = input.split('\n',)
+console.log(data.length)
+let results = [0]
+for (const line of data) {
+    const num = line ? parseInt(line) : 0
+    if (num && !Number.isNaN(num)) {
+        results[results.length - 1] = results[results.length - 1] + num
+    } else {
+        console.log('else', num)
+        results.push(num)
+    }
+}
+
+
+results = results.sort((a, b) =>
+    a - b
+)
+console.log(results[0], results[results.length - 1]);
